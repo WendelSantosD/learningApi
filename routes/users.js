676eb6e9f398b3cde7b6492c6d80d84da0c169fb
WendelSantosD <wendelwcsantos@gmail.com>
2021-01-06 12:28:19 -0300

    Aula 12 - Login + JWT Token

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mysql = require('../mysql').pool
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 router.post('/register', (req, res, next) => {
     mysql.getConnection((err, conn) => {
@@ -56,6 +57,56 @@ router.post('/register', (req, res, next) => {
             }
             
             )    
+    })
+})
+
+router.post('/login', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({
+                error: error
+            })
+        }
+        const query = 'SELECT * FROM users WHERE email = ?'
+        conn.query(query, [req.body.email], (error, results, fields) => {
+            conn.release()
+            if (error) {
+                return res.status(500).send({
+                    error: error
+                })
+            }
+            if(results.length < 1) {
+                return res.status(401).send({
+                    message: 'Authentication failed'
+                })
+            }
+
+            bcrypt.compare(req.body.password, results[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).send({
+                        message: 'Authentication failed'
+                    })
+                }
+                if (result) {
+                    const token = jwt.sign({
+                        id_user: results[0].id_user,
+                        email: results[0].email
+                    }, 'process.env.JWT_KEY',
+                    {
+                        expiresIn: "1h"
+                    }
+                    )
+                    return res.status(200).send({
+                        message: 'Successfully authenticated',
+                        token: token
+                    })
+                }
+
+                return res.status(401).send({
+                    message: 'Authentication failed'
+                })
+            })
+        })
     })
 })
 
